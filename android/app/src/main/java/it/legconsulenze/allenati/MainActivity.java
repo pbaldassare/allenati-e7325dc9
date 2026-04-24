2d49b933781ce721437935d9a895f6433f57cef9
Luca Daniele <luca.daniele.2@gmail.com>
2026-04-24 13:35:50 +0200

    Progetti aggiornati

---
+++ android/app/src/main/java/it/legconsulenze/allenati/MainActivity.java
@@ -0,0 +1,5 @@
+package it.legconsulenze.allenati;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'it.legconsulenze.allenati',
-  appName: 'Allenati',
+  appName: 'Allenati.me',
   webDir: 'dist',
   plugins: {
     SplashScreen: {
